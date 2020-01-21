e58ae694deae07415e0dfe3466d5fc1bbd0ec91c
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-01-20 22:26:55 -0800

    second commit populating city name and temp + connected w api

--- script.js
+++ script.js
@@ -15,21 +15,45 @@ $(document).ready(function () {
     $("#submitBtn").click(function () {
             // asking user for a city name
             var cityName = $("#nameInput").val();
-            cities.push(cityName);
+            if (cityName == "") {
+                alert("please enter the city")
+            }
+
+            else {
+                cities.push(cityName);
+
+                localStorage.setItem("name", JSON.stringify(cities));
+                var storedCities = JSON.parse(localStorage.getItem("name"))
+    
+                $("#cityName").text(storedCities);
+
+                var APIKey = "000a4bf7937f19571d8379dba047c801";
+                var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
+
+
+                $.ajax({
+                            url: queryURL,
+                            method: "GET"
+                        }).then(function (response) {
+                            console.log(response);
+                            var temp = $('.temp');
+                            temp.html(response.list[0].main.temp)
+                            var humidity = $('.humidity');
+                            var wind = $(".wind");
+                            var uvInd = $(".uvInd");
+
+                        });
+            }
+            
+
            
-
-            localStorage.setItem("name", JSON.stringify(cities));
-
-            // if (cityName == null) {
-            //     console.log(cityName)
-            // }
     
         // else {
 
         //     //API key. Add your own API key between the ""
         //     var APIKey = "000a4bf7937f19571d8379dba047c801";
         //     // gettign answer via query url
-        //     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
+        //     
         //     $.ajax({
         //         url: queryURL,
         //         method: "GET"
